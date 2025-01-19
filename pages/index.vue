4a465016a5c90d42628423a78ddae1cf620bb139
Kevin Liu <kevin07liu@gmail.com>
2025-01-19 20:00:04 +0800

    add middleware fn

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,8 @@
-<template>This is Hello, worlda nuxt3 project</template>
+<template>
+  <NuxtLink class="underline font-medium" :href="firstLesson.path">
+    Go To first lesson
+  </NuxtLink>
+</template>
+<script setup>
+const firstLesson = useFirstLesson();
+</script>
